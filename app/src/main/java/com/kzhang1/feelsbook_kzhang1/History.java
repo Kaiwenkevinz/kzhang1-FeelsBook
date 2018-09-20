b50e1f840caafd538dae1dcd82bb637e0634840e
Kaiwenkevinz <kevin.zhangkw@gmail.com>
2018-09-19 23:55:40 -0600

    sample ListView done. Need to implement custom listView

--- app/src/main/java/com/kzhang1/feelsbook_kzhang1/History.java
+++ app/src/main/java/com/kzhang1/feelsbook_kzhang1/History.java
@@ -1,17 +1,33 @@
 package com.kzhang1.feelsbook_kzhang1;
 
 
+import android.icu.util.ValueIterator;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static android.R.layout.simple_list_item_1;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class History extends Fragment {
+
+
 
 
     public History() {
@@ -22,8 +38,16 @@ public class History extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false);
+
+        String[] names = {"kevin", "nolan", "jack"};
+        View v = inflater.inflate(R.layout.fragment_history, container, false);
+
+        ListView listViewNames = (ListView) v.findViewById(R.id.history_listView);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, names);
+        listViewNames.setAdapter(adapter);
+
+        return v;
+
     }
 
 }
