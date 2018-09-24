e54138ec61526fa4491d0582b712ff79653a078b
Kaiwenkevinz <kevin.zhangkw@gmail.com>
2018-09-24 00:14:30 -0600

    home alert dialog done

--- app/src/main/java/com/kzhang1/feelsbook_kzhang1/Home.java
+++ app/src/main/java/com/kzhang1/feelsbook_kzhang1/Home.java
@@ -1,11 +1,20 @@
 package com.kzhang1.feelsbook_kzhang1;
 
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -20,10 +29,46 @@ public class Home extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
+        View v;
+        Button button_love;
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        v = inflater.inflate(R.layout.fragment_home, container, false);
+        button_love = (Button) v.findViewById(R.id.button_love);
+        final TextView textView_userInput = (TextView) v.findViewById(R.id.textView_userInput);
+
+        button_love.setOnClickListener(new View.OnClickListener() {
+            EditText editTextt_userInput;
+            @Override
+            public void onClick(View v) {
+                View view_user_input = inflater.inflate(R.layout.user_input, container, false);
+                editText_userInput = (EditText) view_user_input.findViewById(R.id.editText_userInput);
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getContext());
+                alertBuilder.setView(view_user_input);
+                alertBuilder.setCancelable(true);
+                alertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        textView_userInput.setText(editText_userInput.getText());
+                    }
+                });
+                alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                Dialog dialog = alertBuilder.create();
+                dialog.show();
+            }
+        });
+
+
+
+        return v;
     }
 
 }
