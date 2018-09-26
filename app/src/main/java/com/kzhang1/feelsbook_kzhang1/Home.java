38a0e5bf79928ea0823ef820bae35ba9bc3a4b81
Kaiwenkevinz <kevin.zhangkw@gmail.com>
2018-09-25 18:12:55 -0600

    display time

--- app/src/main/java/com/kzhang1/feelsbook_kzhang1/Home.java
+++ app/src/main/java/com/kzhang1/feelsbook_kzhang1/Home.java
@@ -22,7 +22,11 @@ import com.google.gson.reflect.TypeToken;
 import org.w3c.dom.Text;
 
 import java.lang.reflect.Type;
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import static android.content.Context.MODE_PRIVATE;
@@ -53,19 +57,34 @@ public class Home extends Fragment {
 
         button_love.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(final View v) {
                 // editText_userInput from user_input.xml
-                View view_user_input = inflater.inflate(R.layout.user_input, container, false);
+                final View view_user_input = inflater.inflate(R.layout.user_input, container, false);
                 final EditText editText_userInput = (EditText) view_user_input.findViewById(R.id.editText_userInput);
+
+                DateFormat df = new SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z");
+                String date = df.format(Calendar.getInstance().getTime());
+                TextView textView_date = (TextView) view_user_input.findViewById(R.id.textView_date);
+                textView_date.setText(date);
 
                 AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getContext());
                 alertBuilder.setView(view_user_input);
                 alertBuilder.setCancelable(true);
-
                 alertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         SharedPreference sharedPreference = new SharedPreference();
+
+                        // collect arguments
+                        //emotion
+                        String emotion = "Love";
+
+
+                        //comment
+                        String comment = editText_userInput.getText().toString();
+
+                        // generate emotion object
+
                         Emotion new_emotion = new Emotion("love", "3", editText_userInput.getText().toString());
                         sharedPreference.savePreference(getActivity(), new_emotion);
                     }
@@ -74,8 +93,6 @@ public class Home extends Fragment {
                 alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-//                        Log.d("home", mEmotionList.toString());
-//                        System.out.println(mEmotionList);
                     }
                 });
                 Dialog dialog = alertBuilder.create();
